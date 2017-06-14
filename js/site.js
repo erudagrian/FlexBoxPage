782a805d8acdecee2feacca97f3698ed1acb54d1
erudagrian <galathisrael@yahoo.com.mx>
2017-06-14 18:24:19 -0500

    First Commit

---
+++ js/site.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  // Mobile menu toggle
+  $('.nav-toggle').click(function(){
+    $('.main-nav').toggleClass('is-open')
+    $('.hamburger').toggleClass('is-open')
+  });
+
+});
